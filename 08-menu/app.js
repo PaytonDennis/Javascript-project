46e888752e2da113401ad10d92525f8ff31fdcf9
PaytonDennis <102490243+PaytonDennis@users.noreply.github.com>
2022-04-07 22:59:20 -0500

    finished menu

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -1,4 +1,4 @@
-//items
+//define menu items as a constant and fill them with data and give id #
 const menu = [
   {
     id: 1,
@@ -14,15 +14,15 @@ const menu = [
     category: "lunch",
     price: 13.99,
     img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: `Classic cheeseburger and fries served with ketchup `,
   },
   {
     id: 3,
-    title: "godzilla milkshake",
+    title: "birthday cake shake",
     category: "shakes",
     price: 6.99,
     img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: `house special birthday cake milkshake for your birthday or any occasion really. `,
   },
   {
     id: 4,
@@ -30,31 +30,31 @@ const menu = [
     category: "breakfast",
     price: 20.99,
     img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: `sunny side up eggs, toast, and hash browns served with coffee. `,
   },
   {
     id: 5,
-    title: "egg attack",
+    title: "hen-house burger",
     category: "lunch",
     price: 22.99,
     img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: `Cheeseburger severed with a fluffy egg inside on a sesame seed bun. `,
   },
   {
     id: 6,
-    title: "oreo dream",
+    title: "oreo shake",
     category: "shakes",
     price: 18.99,
     img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: `A shake made with real Oreos and fudge. Garnished with a strawberry. `,
   },
   {
     id: 7,
-    title: "bacon overflow",
+    title: "Country biscuit",
     category: "breakfast",
     price: 8.99,
     img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: `A classic bacon, lettuce, and egg breakfast biscuit served on a cheddar cheese bun. `,
   },
   {
     id: 8,
@@ -62,24 +62,67 @@ const menu = [
     category: "lunch",
     price: 12.99,
     img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: `100% all-American burger with house-cut fries served with ketchup.  `,
   },
-  {
-    id: 9,
-    title: "quarantine buddy",
-    category: "shakes",
-    price: 16.99,
-    img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-  },
+  
 ];
 
 
 //section center
 const sectionCenter = document.querySelector('.section-center');
 
-window.addEventListener("DOMContentLoaded", function(){
-  console.log("shake and bake");
+//buttons filter 
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+// load items
+window.addEventListener("DOMContentLoaded", function() {
+  displayMenuItems(menu)
+});
+
+// filter items
+filterBtns.forEach(function(btn) {
+  btn.addEventListener('click', function (e) {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+    if(menuItem.category === category) {
+       return menuItem;
+       }
+    });
+    if(category === 'all'){
+      displayMenuItems(menu)
+    }
+    else{
+      displayMenuItems(menuCategory);
+    }
+  });
+});
+
+  //create function for filtering with menuItems array as parameter
+function displayMenuItems(menuItems){
+  let  displayMenu =  menuItems.map(function(item){
+
+    //add string with html syntax & dynamically populate
+    return ` <article class="menu-item">
+    <img src=${item.img} class="photo"
+     alt=${item.title}/>
+     <div class="item-info">
+       <header>
+        <h4> ${item.title} </h4>
+        <h4 class="price">: $${item.price}</h4> 
+       </header>
+     <p class="item-text">
+        ${item.desc}
+      </p>
+     </div>
+  </article>`;
+  
+    });
+  
+    //use join to link all data together
+    displayMenu =displayMenu.join("")
   
   
-  })
+    //add data to section center
+    sectionCenter.innerHTML = displayMenu
+    
+}
